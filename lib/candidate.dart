718fbeec673e8aeff019b9a1cf0b3d7b09d49e55
Donialimbaev <48401846+Donialimbaev@users.noreply.github.com>
2022-10-22 19:20:34 +0600

    card color changing added, candidate model added, dummy data added

---
+++ lib/candidate.dart
@@ -0,0 +1,13 @@
+class Candidate {
+  final int number;
+  final String firstname;
+  final String lastname;
+  final String patronymic;
+
+  Candidate({
+    required this.number,
+    required this.firstname,
+    required this.lastname,
+    required this.patronymic,
+  });
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,51 @@
+import 'candidate.dart';
+
+List<Candidate> alliance = [
+  Candidate(
+      number: 1,
+      firstname: "Мирлан",
+      lastname: "Жеенчороев",
+      patronymic: "Канатбекович"),
+  Candidate(
+      number: 2,
+      firstname: "Жанарбек",
+      lastname: "Акаев",
+      patronymic: "Кубанычович"),
+  Candidate(
+      number: 3,
+      firstname: "Чингиз",
+      lastname: "Айдарбеков",
+      patronymic: "Азаматович"),
+  Candidate(
+      number: 4,
+      firstname: "Токтобубу",
+      lastname: "Оргалча",
+      patronymic: "Турдубаевич"),
+  Candidate(
+      number: 5, firstname: "Алтынбек", lastname: "Сулайманов", patronymic: ""),
+  Candidate(
+      number: 6,
+      firstname: "Фарходбек",
+      lastname: "Фарходбек",
+      patronymic: "Айбекович"),
+  Candidate(
+      number: 7,
+      firstname: "Эльдар",
+      lastname: "Абакиров",
+      patronymic: "Курманбекович"),
+  Candidate(
+      number: 8,
+      firstname: "Айсулуу",
+      lastname: "Мамашова",
+      patronymic: "Турусбековна"),
+  Candidate(
+      number: 9,
+      firstname: "Максатбек",
+      lastname: "Сулайманов",
+      patronymic: "Жолдошбекович"),
+  Candidate(
+      number: 10,
+      firstname: "Жолдошбек",
+      lastname: "Кожобеков",
+      patronymic: "Асанович"),
+];
